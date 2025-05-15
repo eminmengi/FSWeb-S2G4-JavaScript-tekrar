80127032c466fce8fad927d2d728ca037a6cfb44
Emin Mengi <eminmengi@gmail.com>
2025-05-15 21:08:21 +0300

    3a- En büyük ve En küçük sayılar bulundu

--- index.js
+++ index.js
@@ -103,7 +103,18 @@ let ucetambolunenler,
 
 // 3a çözümü
 
-/* kodlar buraya */
+enbuyuk = sayilar[0];
+enkucuk = sayilar[0];
+for (let i =1; i < sayilar.length; i++){
+  if (sayilar[i] > enbuyuk){
+    enbuyuk = sayilar[i];
+  }
+  if (sayilar[i] < enkucuk){
+    enkucuk = sayilar[i];
+  }
+}
+console.log("En büyük sayı: ", enbuyuk);
+console.log("En küçük sayı: ", enkucuk);
 
 // 3b çözümü:
 
